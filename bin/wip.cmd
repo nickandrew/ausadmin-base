c811869eb7b9dac81471fa6d44d5f468af751440
Nick Andrew <nick@nick-andrew.net>
1998-04-18 14:52:14 +0000

    Produces wip list
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@5 8ab3fd6c-811f-0410-acb7-fe4950304a8b

---
+++ bin/wip.cmd
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+#	Emit a series of links to newsgroups work in progress
+
+chdir "../Wip";
+$count = 0;
+for $fn (<*>) {
+	open(F, "<$fn") || next;
+	undef %data;
+
+	while (<F>) {
+		chop;
+		last if (/^$/);
+		($key, $value) = ($_ =~ /([^:]*):\s+(.*)/);
+		print "key: $key, value: /$value/\n";
+		$data{$key} = $value;
+	}
+
+	close(F);
+	$key = $fn;
+	if ($data{Updated} ne "") {
+		$key = "$data{Updated} $fn";
+	}
+
+	$keys{$key} = 1;
+	if ($data{Name} ne "") {
+		$text{$key} = $data{Name};
+	} else {
+		$text{$key} = $fn;
+	}
+
+	$fn{$key} = $fn;
+	$updated{$key} = $data{Updated};
+	$link{$key} = $data{Link};
+	$status{$key} = $data{Status};
+}
+
+# Emit gathered info
+for $i (sort (keys %keys)) {
+	print "<li>";
+	if ($link{$key} ne "n") {
+		printf "<a href=\"%s\">", ($link{$key} eq "" ? "/Wip/$fn{$key}" : $link{$key});
+	}
+	print $text{$key};
+	if ($link{$key} ne "n") {
+		printf "</a>";
+	}
+	if ($updated{$key} ne "") {
+		print " Updated: $updated{$key}";
+	}
+	print "\n";
+	$count++;
+}
+
+if ($count == 0) {
+	print "<li>None at present\n";
+}
