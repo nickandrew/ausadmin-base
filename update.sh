e8a3bfc63905c5e9081a60e29e81e52f514ec2a6
Nick Andrew <nick@nick-andrew.net>
1998-04-22 03:26:20 +0000

    50->10 initially
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@10 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- update.sh
+++ update.sh
@@ -3,5 +3,15 @@
 package="http://aus.news-admin.org/"
 dest="ausadmin@ultraman:/virt/web/ausadmin/"
 
+# Fix permissions if necessary
+for i in `find . \! -perm -4 -print` ; do
+	echo Fixing permissions: `ls -ld $i`
+	if [ -x $i ] ; then
+		chmod go+rx $i
+	else
+		chmod go+r $i
+	fi
+done
+
 echo Updating $package to $dest
 rsync -C -v -a -e ssh --delete * $dest
