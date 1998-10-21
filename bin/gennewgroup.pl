24dc510e20a4ba771b134e70199795e8ecebd93a
David Formosa <dformosa@zeta.org.au>
1998-10-21 04:46:41 +0000

    *** empty log message ***
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@88 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/gennewgroup.pl
+++ bin/gennewgroup.pl
@@ -72,7 +72,7 @@ sub makemessage ( $$ ) {
 	  $post .= "\nThis control message has been forged as \"$from\" for the benefit of those\nsites still honouring his posts.  If you are one of those sites please see \<URL:http://aus.news-admin.org/\>.";
      }
 
-#     open FILE,">newgroup.$name" or die "Yipe Yipe Yipe unable to open $name because $!\n";
+     open FILE,">~/vote/newgroup.$name" or die "Yipe Yipe Yipe unable to open $name because $!\n";
      print $post;
      
 }
