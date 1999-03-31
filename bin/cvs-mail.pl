8060459519ba834c21e3be351f971da7cf908a96
David Formosa <dformosa@zeta.org.au>
1999-03-31 08:45:33 +0000

    This is a redirector into the task system.
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@122 8ab3fd6c-811f-0410-acb7-fe4950304a8b

---
+++ bin/cvs-mail.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+# Usage: cvs-mail.pl filename
+
+# Proccesses incoming mail for ausadmin adding
+
+# X-PTS-Account: ausadmin
+# X-PTS-Status: open
+# X-PTS-Handled: dformosa
+
+my $ttsid = 'zz-pts@staff.zeta.org.au';
+my $admin = 'dformosa@zeta.org.au';
+my $from  = 'ausadmin@zeta.org.au';
+
+open (MAIL,'|/usr/sbin/sendmail $ttsid') or 
+  die "Can't fork sendmail $! please report this to $admin";
+
+#Set MAIL as the defult output for print;
+
+select MAIL;
+
+while (<>) {
+  print;
+  last if /^$/;
+}
+
+print "X-PTS-Account: ausadmin\n";
+print "X-PTS-Status: open\n";
+print "X-PTS-Handled: dformosa\n";
+
+print "\n";
+
+while (<>) {
+  print;
+}
+
+close MAIL or die "Can't open sendmail $! please report this to $admin";
