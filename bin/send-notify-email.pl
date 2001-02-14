7ffb8b0f0ebf785b59a4139817c09bcd0b9fa92a
Nick Andrew <nick@nick-andrew.net>
2001-02-14 13:30:03 +0000

    Creates a script to pipe to /bin/bash which calls sendmail to post emails to voters
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@183 8ab3fd6c-811f-0410-acb7-fe4950304a8b

---
+++ bin/send-notify-email.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+#	@(#) send-notify-email.pl - Send email to all voters
+#	Usage: send-notify-email.pl newsgroup-name filename-to-send
+
+use lib '.', 'bin';
+use Vote;
+use Newsgroup;
+
+my $newsgroup = shift @ARGV;
+my $filename = shift @ARGV;
+
+die "Invalid newsgroup name <$newsgroup>" if (!Newsgroup::validate($newsgroup));
+
+my $vote = new Vote(name => $newsgroup);
+my $vote_list = $vote->get_tally();
+
+# $vote_list = [$v, $v, $v, ...]
+# $v = { email => email_address, group => newsgroup, vote => YES|NO|ABSTAIN, ts => 987654321 }
+
+my $ng_dir = $vote->ng_dir();
+
+if (!-f "$ng_dir/$filename") {
+	die "No such file to send: $ng_dir/$filename";
+}
+
+# Read in the file
+
+my $s = $vote->read_file($filename);
+
+# Now send it out to each vote in the tally file
+
+foreach my $v (@$vote_list) {
+	my $email = $v->{email};
+
+	print "/usr/sbin/sendmail $email <<'__EOF__'\n";
+	# We set the To: address for each one
+	print "To: $email\n";
+	# Imagine some pgp signing shit in here
+	print join('', @$s);
+	print "__EOF__\n\n";
+}
+
+exit(0);
