8801c32e6cab5d209617dc7e36a0a8cc2dafd4ce
Nick Andrew <nick@nick-andrew.net>
2001-02-20 13:25:28 +0000

    Keep state so we don't keep checking known voters
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@198 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/check-voters.pl
+++ bin/check-voters.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#	@(#) check-voters.pl [-r] newsgroup-name < message-file | /bin/bash
+#	@(#) check-voters.pl newsgroup-name voters-file < message-file | /bin/bash
 #
 # $Source$
 # $Revision$
@@ -11,8 +11,19 @@ use Vote;
 use Newsgroup;
 
 my $newsgroup = shift @ARGV || die "No newsgroup name supplied";
+my $voters_file = shift @ARGV || die "No voters file supplied";
 
 die "Invalid newsgroup name <$newsgroup>" if (!Newsgroup::validate($newsgroup));
+
+my %voter_state;
+
+open(VF, "<$voters_file") or die "Unable to open $voters_file: $!";
+while (<VF>) {
+	chomp;
+	my($email,$state) = split;
+	$voter_state{$email} = $state;
+}
+close(VF);
 
 my $vote = new Vote(name =>$newsgroup);
 my $ng_dir = $vote->ng_dir();
@@ -25,6 +36,7 @@ if (!open(V, "<$tally_path")) {
 }
 
 my @recipients;
+my $now = time();
 
 while (<V>) {
 	chomp;
@@ -32,12 +44,32 @@ while (<V>) {
 
 	# vote is yes/no/abstain/forge (mostly uppercase)
 	# only yes and no votes affect the result.
-	if ($vote =~ /^(yes|no)$/i) {
-		push(@recipients, [$email, $timestamp]);
+	next unless ($vote =~ /^(yes|no)$/i);
+
+	# Now check if we checked them before
+	if (exists $voter_state{$email} && $voter_state{$email} =~ /^\d+$/) {
+		# It's ok if check was less than 90 days ago
+		next if ($now - $voter_state{$email} < 86400 * 90);
+	}
+
+	# Otherwise, need to check them
+	push(@recipients, [$email, $timestamp]);
+
+	# And mark they've been checked
+	# KLUDGE ... this is dodgy ... what do we do when they fail a check?
+	if (!exists $voter_state{$email}) {
+		$voter_state{$email} = $now;
 	}
 }
 
 close(V);
+
+# Now update the voter_state file
+open(VF, ">$voters_file") or die "Unable to open $voters_file for write: $!";
+foreach my $email (sort (keys %voter_state)) {
+	print VF "$email $voter_state{$email}\n";
+}
+close(VF);
 
 # Now read the header and body of the message
 
@@ -64,11 +96,10 @@ sub sendmail {
 	print "/usr/sbin/sendmail $recipient <<'__EOF__'\n";
 
 	print "To: $recipient\n";
-
 	print @$msg_ref;
 	print "__EOF__\n\n";
 }
 
 sub usage {
-	die "Usage: check-voters.pl newsgroup-name < message-file | /bin/bash\n";
+	die "Usage: check-voters.pl newsgroup-name voters-file < message-file | /bin/bash\n";
 }
