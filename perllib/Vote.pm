52d3c54de6d5ed2c087cccb8a620aed5c2a0f5f7
Nick Andrew <nick@nick-andrew.net>
2001-03-17 10:18:14 +0000

    extend Vote.pm to calculate current state for each vote
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@220 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- perllib/Vote.pm
+++ perllib/Vote.pm
@@ -1,4 +1,6 @@
 #	@(#) Vote.pm - a vote of some kind
+#
+# $Id$
 
 =head1 NAME
 
@@ -6,9 +8,17 @@ Vote - a Vote of some kind
 
 =head1 SYNOPSIS
 
-use Vote;
+ use Vote;
 
-my $vote = new Vote(name => 'aus.history');
+ my $vote = new Vote(name => 'aus.history');
+ $vote_dir = $vote->ng_dir();		# returns relative path of vote's dir
+ $time = $vote->get_start_time();
+ $time = $vote->get_end_time();
+ $time = $vote->get_cancel_time();
+ $list_ref = $vote->read_file($filename);
+ $list_ref = $vote->get_distribution();
+ $list_ref = $vote->get_tally();
+ $string = $vote->state();		# return string representing vote's state
 
 =cut
 
@@ -63,6 +73,10 @@ sub _read_file {
 
 sub ng_dir {
 	my $self = shift;
+
+	if (exists $self->{ng_dir}) {
+		return $self->{ng_dir};
+	}
 
 	my $ng_dir = "$self->{vote_dir}/$self->{name}";
 
@@ -129,6 +143,106 @@ sub get_tally {
 	}
 
 	return \@list;
+}
+
+$Vote::expected_files = {
+	'distribution' => 1,
+	'charter' => 1,
+	'rationale' => 1,
+	'ngline' => 1,
+	'proposer' => 1,
+	'vote_start.cfg' => 1,
+	'vote_cancel.cfg' => 1,
+	'rfd_posted.cfg' => 1,
+	'cfv-notes.txt' => 1,
+	'cancel-email.txt' => 1,
+	'cancel-notes.txt' => 1,
+	'group.creation.date' => 1,
+	'post.real' => 1,
+	'post.fake.phil' => 1,
+	'post.fake.robert' => 1,
+	'control.msg' => 1,
+	'rfd' => 1,
+	'voterule' => 1,
+	'endtime.cfg' => 1,
+	'cfv' => 1,
+	'posted.cfv' => 1,
+	'posted.cfg' => 1,
+	'result' => 1,
+	'tally.dat' => 1,
+};
+
+# Return a list of unexpected filenames in a vote directory
+
+sub check_files {
+	my $self = shift;
+
+	my $ng_dir = $self->ng_dir();
+
+	opendir(D, $ng_dir);
+	my @files = grep { ! /^\./ } readdir(D);
+	closedir(D);
+
+	my @list;
+
+	foreach my $f (@files) {
+		next if (exists $Vote::expected_files->{$f});
+		push(@list, $f);
+	}
+
+	return \@list;
+}
+
+
+# $string = $vote->state() ...
+
+sub state {
+	my $self = shift;
+
+	my $ng_dir = $self->ng_dir();
+
+	if (!-f "$ng_dir/rfd") {
+		return "new";
+	}
+
+	if (!-f "$ng_dir/voterule") {
+		return "rfd";
+	}
+
+	if (!-f "$ng_dir/endtime.cfg") {
+		return "vote/notsetup";
+	}
+
+	if (!-f "$ng_dir/cfv") {
+		return "vote/nocfv";
+	}
+
+	if (!-f "$ng_dir/posted.cfv") {
+		return "vote/unsignedcfv";
+	}
+
+	if (!-f "$ng_dir/posted.cfg") {
+		return "vote/cfvnotposted";
+	}
+
+	my $state = "vote/running";
+
+	my $end_time = $self->get_end_time();
+	my $now = time();
+
+	if ($now < $end_time) {
+		return $state;
+	}
+
+	# Vote period must be finished.
+
+	# note this is testing for presence of file, not absence
+	if (-f "$ng_dir/result") {
+		return "complete/result";
+	}
+
+	# cop-out
+	return "complete/checking";
 }
 
 1;
