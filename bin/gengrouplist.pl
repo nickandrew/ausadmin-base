536d317095eec573ff9812e3196b603e0945b4f6
Nick Andrew <nick@nick-andrew.net>
2001-04-02 12:48:14 +0000

    Change how the group list is generated and signed
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@229 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/gengrouplist.pl
+++ bin/gengrouplist.pl
@@ -8,39 +8,30 @@
 require "bin/postheader.pli";
 require "bin/misc.pli";
 
-$now = time;
-$postaddress = "ausadmin\@aus.news-admin.org";
-$grouplist = "data/ausgroups";
-
-if (-f "/usr/bin/pgps") {
-     $pgpcmd = "pgps -fat";
-} else {
-     $pgpcmd = "pgp -fast";
-}
-
-select(STDOUT); $| = 1;
+my $postaddress = "ausadmin\@aus.news-admin.org";
+my $grouplist = "data/ausgroups";
 
 if (! -f $grouplist) {
-     die "gengrouplist.pl: No list of newsgroups: $grouplist\n";
+	die "gengrouplist.pl: No list of newsgroups: $grouplist\n";
 }
 
-$grouplist_header = readfile("data/grouplist.header");
-$grouplist_footer = readfile("data/grouplist.footer");
+my $grouplist_header = readfile("data/grouplist.header");
+my $grouplist_footer = readfile("data/grouplist.footer");
 
-($sec,$min,$hour,$mday,$mon,$year,$wday,$isdst) = localtime(time);
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$isdst) = localtime(time);
 $year += 1900; $mon++;
-$now = sprintf "%d-%02d-%02d %02d:%02d:%02d", $year, $mon, $mday, $hour, $min, $sec;
+my $ts = sprintf "%d-%02d-%02d %02d:%02d:%02d", $year, $mon, $mday, $hour, $min, $sec;
 
 # Generate the message, header first
 
-%header = (
-	   'Subject' => "List of aus.* newsgroups at $now",
+my %header = (
+	   'Subject' => "List of aus.* newsgroups at $ts",
 	   'Newsgroups' => 'aus.net.news'
 	  );
 
 # Open the groups file
 if (!open(C, "<$grouplist")) {
-     die "Unable to open $grouplist: $!\n";
+	die "Unable to open $grouplist: $!\n";
 }
 
 print_header(\%header);
@@ -48,22 +39,16 @@ print_header(\%header);
 # Can't send this in @body because an extra blank line will be added
 # push(@body, $grouplist_header);
 
-push(@body, $grouplist_footer);
+# push(@body, $grouplist_footer);
 
-if (!open(P, "|$pgpcmd")) {
-     die "Unable to open pipe to pgp!\n";
-}
-
-print P $grouplist_header;
+print $grouplist_header;
 
 while (<C>) {
-     print P $_;
+	print $_;
 }
 close(C);
 
-print P $grouplist_footer;
-
-close(P);
+print $grouplist_footer;
 
 exit(0);
 
