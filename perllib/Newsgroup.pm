1ca4d40cde0af4c9235155347a98a101ef50b64f
Nick Andrew <nick@nick-andrew.net>
2001-05-29 12:13:37 +0000

    Get the group info, as known to the local newsserver
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@265 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- perllib/Newsgroup.pm
+++ perllib/Newsgroup.pm
@@ -8,9 +8,14 @@ Newsgroup - a newsgroup
 
 use Newsgroup;
 
-my $vote = new Newsgroup(name => 'aus.history');
+ $nntp = new Net::NNTP(...)
 
-my $bool = Newsgroup::validate('aus.history');
+ $ng = new Newsgroup(name => 'aus.history', [nntp_server=>$nntp]);
+ $bool = Newsgroup::validate('aus.history');
+
+ $ng->set_server($nntp)
+ $flags = $ng->group_flags()  ... return the 'y' or 'm' status of the group
+ 
 
 =cut
 
@@ -33,6 +38,69 @@ sub validate {
 
 	return 1 if ($ng_name =~ /^[a-z0-9+-]+\.[a-z0-9+-]+(\.[a-z0-9+-]+)*(:\d\d\d\d-\d\d-\d\d)?$/);
 	return 0;
+}
+
+sub set_server {
+	my $self = shift;
+	my $conn = shift;
+
+	die "Newsgroup::set_server must be given a reference" if (!ref $conn);
+
+	$self->{nntp_server} = $conn;
+
+	return $conn;
+}
+
+# Get newsgroup info. Return a hash ref: art_high, art_low, flags
+
+sub group_info {
+	my $self = shift;
+
+	die "Newsgroup::group_info() called without a server set" if (!ref $self->{nntp_server});
+
+	my $name = $self->{name};
+
+	my $ref = $self->{nntp_server}->active($name);
+
+	return undef if (!ref $ref);
+
+	my $list = $ref->{$name};
+
+	$self->{group_info} = {
+		art_high => $list->[0],
+		art_low => $list->[1],
+		flags => $list->[2]
+	};
+
+	return $ref->{$self->{name}};
+}
+
+# return 'y' for unmoderated, or 'm' for moderated group
+
+sub group_flags {
+	my $self = shift;
+
+	if (!exists $self->{group_info}) {
+		$self->group_info();
+	}
+
+	return $self->{group_info}->{flags};
+}
+
+# temp procedure
+
+sub debug_it {
+	my $ref = shift;
+	print "Ref is $ref\n";
+	foreach my $v (keys %$ref) {
+		print "Key: $v ";
+		my $l = $ref->{$v};
+		foreach (@$l) {
+			print " val $_";
+		}
+		print "\n";
+	}
+	print "\n";
 }
 
 1;
