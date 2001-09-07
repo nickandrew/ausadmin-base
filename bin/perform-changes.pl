cfde67e6667bc8870dbab7462d3e4be0c24435f3
Nick Andrew <nick@nick-andrew.net>
2001-09-07 13:12:48 +0000

    Now it can generate an appropriate control message and write to control*.msg
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@330 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/perform-changes.pl
+++ bin/perform-changes.pl
@@ -2,6 +2,7 @@
 
 use lib 'bin';
 
+use Ausadmin;
 use Vote;
 use Newsgroup;
 
@@ -38,15 +39,15 @@ if (%$hr) {
 
 # Now go through each change ...
 my $ct_map = {
-	'moderate' => \&do_moderate,
-	'unmoderate' => \&do_unmoderate,
+	'moderate' => \&do_control,
+	'unmoderate' => \&do_control,
 	'charter' => \&do_charter,
-	'newgroup' => \&do_newgroup,
-	'rmgroup' => \&do_rmgroup,
+	'newgroup' => \&do_control,
+	'rmgroup' => \&do_control,
 };
 
 foreach my $c_hr (@change_list) {
-	print "Change type: $c_hr->{type}\n";
+	print STDERR "Change type: $c_hr->{type}\n";
 
 	my $type = $c_hr->{type};
 
@@ -64,10 +65,6 @@ exit(0);
 sub usage {
 	die "Usage: perform-changes.pl vote-name\n";
 }
-
-sub do_moderate { die "Unimplemented moderate.\n"; }
-
-sub do_unmoderate { die "Unimplemented unmoderate.\n"; }
 
 sub do_charter {
 	my $v = shift;
@@ -87,6 +84,151 @@ sub do_charter {
 	}
 }
 
-sub do_newgroup { die "Unimplemented newgroup.\n"; }
+sub do_control {
+	my $v = shift;
+	my $change_hr = shift;
+	my $type = 'post.real';		# KLUDGE ... force non-forged msgs only
 
-sub do_rmgroup { die "Unimplemented rmgroup.\n"; }
+	my $vote = $v->{name};
+
+	my $text = make_control($type, $vote, $change_hr);
+	my $vote_dir = $v->ng_dir();
+	my $control_path = "$vote_dir/control.msg";
+
+	if (-f $control_path) {
+		# KLUDGE ... race condition!
+		my $index = 1;
+		while (-f $control_path) {
+			$index++;
+			$control_path = "$vote_dir/control$index.msg";
+		}
+	}
+
+	if (!open(CF, ">$control_path")) {
+		die "Unable to open $control_path for write: $!";
+	}
+
+	print CF $text;
+
+	close(CF);
+
+	$v->audit("Wrote control message($change_hr->{type}, $type, $vote) into $control_path");
+}
+
+sub make_control {
+	my $type = shift;
+	my $vote = shift;
+	my $change_hr = shift;
+
+	my $newgroup = 0;
+	my $rmgroup = 0;
+	my $moderated = 0;
+
+	if ($change_hr->{type} =~ /^(moderate|unmoderate|newgroup)$/) {
+		$newgroup = 1;
+		$rmgroup = 0;
+	}
+
+	if ($change_hr->{type} =~ /^(rmgroup)$/) {
+		$newgroup = 0;
+		$rmgroup = 1;
+	}
+
+	if ($change_hr->{mod_status} eq 'm') {
+		$moderated = 1;
+	}
+
+#Get the from line for the forging/setting for the from and approved lines
+#should most likely be read from a file, just sticking it here while I decide
+#where it should go.
+
+	my $from = {
+		"post.real" => "Aus news admin <ausadmin\@aus.news-admin.org>",
+		"post.fake.phil" => "Phil Herring <revdoc\@cs.uow.edu.au>",
+		"post.fake.robert" => "kre\@munnari.OZ.AU (Robert Elz)",
+	} -> {$type};
+
+	my $post;
+
+	if ($newgroup) {
+		$post = donewgroup($moderated,$from,$vote);
+	} elsif ($rmgroup) {
+		$post = dormgroup($from,$vote);
+	} else {
+		die "not newgroup and not rmgroup - eh?";
+	}
+
+	return $post;
+}
+
+sub donewgroup {
+	my $moderated = shift;
+	my $from = shift;
+	my $vote = shift;
+
+	my $name = $vote;		# KLUDGE
+
+	my $ngline = Ausadmin::read1line("vote/$vote/ngline:$name");
+	my $charter = Ausadmin::readfile("vote/$vote/charter:$name");
+	my $control;
+	my $modname;
+
+	if ($moderated) {
+		$control = "newgroup $name m";
+		$modname = "a moderated";
+	} else {
+		$control = "newgroup $name";
+		$modname = "an unmoderated";
+	}
+
+	my $post = <<"EOF";
+From: $from
+Subject: Cmsg newgroup $name
+Newsgroups: aus.net.news,$name
+Control: $control
+Approved: $from
+
+
+$name is $modname newsgroup which passed its vote for
+creation as reported in aus.net.news.  For full information, see:
+
+	http://aus.news-admin.org/cgi-bin/voteinfo?newsgroup=$vote
+
+For your newsgroups file:
+$ngline
+
+CHARTER: $name
+
+$charter
+END CHARTER.
+EOF
+
+	return $post;
+}
+
+sub dormgroup {
+	my $from = shift;
+	my $vote = shift;
+
+	my $name = $vote;		# KLUDGE
+
+	my $control = "rmgroup $name";
+	my $modname;
+
+	my $post = <<"EOF";
+From: $from
+Subject: Cmsg rmgroup $name
+Newsgroups: aus.net.news,$name
+Control: $control
+Approved: $from
+
+
+ausadmin requests removal of group $name as reported in aus.net.news.
+For full information, see:
+
+	http://aus.news-admin.org/cgi-bin/voteinfo?newsgroup=$vote
+EOF
+
+	return $post;
+}
+
