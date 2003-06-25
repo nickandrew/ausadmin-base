c0ee046f4c78ebc14da2ea2c01622da515387a43
Nick Andrew <nick@nick-andrew.net>
2003-06-25 01:21:00 +0000

    Remove all the static text
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@388 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- perllib/View/MainPage.pm
+++ perllib/View/MainPage.pm
@@ -6,6 +6,7 @@ package View::MainPage;
 use strict;
 
 use VoteList qw();
+use Include qw();
 
 sub output {
 	my $lr = shift;
@@ -242,17 +243,17 @@ sub ausadminContents {
    <table cellpadding=5 cellspacing=0 width=100% border=0>
 EOF
 	push(@contents, ausadminSubHeadingRow("Overview"));
-	push(@contents, ausadminOverviewRow());
+	push(@contents, Include::html('overview.html'));
 	push(@contents, ausadminSubHeadingRow("ausadmin Role"));
-	push(@contents, ausadminRoleRow());
+	push(@contents, Include::html('role.html'));
 	push(@contents, ausadminSubHeadingRow("aus.* structure planning"));
-	push(@contents, ausadminStructureRow());
+	push(@contents, Include::html('structure.html'));
 	push(@contents, ausadminSubHeadingRow("ausadmin Policy"));
-	push(@contents, ausadminPolicyRow());
+	push(@contents, Include::html('policy.html'));
 	push(@contents, ausadminSubHeadingRow("Links"));
-	push(@contents, ausadminLinksRow());
+	push(@contents, Include::html('links.html'));
 	push(@contents, ausadminSubHeadingRow("Newsgroup Creation Proposals"));
-	push(@contents, ausadminCreationProposalsRow());
+	push(@contents, Include::html('proposals.html'));
 	push(@contents, <<EOF);
    </table>
 EOF
@@ -268,170 +269,6 @@ sub ausadminSubHeadingRow {
     <tr bgcolor="#000000">
      <td>
       <font size=+1 color="#66cc66" face=sans-serif><b>$string</b></font>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminOverviewRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <p>The aus.* newsgroup administration is a volunteer effort to maintain
-    and develop the aus.* USENET newsgroup hierarchy. We will achieve this by
-    setting a clear policy for the growth of Australian newsgroups,
-    managing the creation and deletion of newsgroups according to this
-    policy, and providing online information to support the development
-    of Australian newsgroups as a national resource.</p>
-
-      <p>The aus.* administrator is currently
-    Nick Andrew (<a href="http://www.nick-andrew.net">www.nick-andrew.net</a>),
-    and operational staff and facilities are provided by
-    <a href="http://www.tull.net/">Tullnet Pty Ltd</a>.
-
-      <p>We wish to automate the administration task as much as possible.
-    Before sending any questions by email make sure you have read the
-    relevant FAQs and are aware of our current policy. This web site
-    should cover most, if not all, queries you may have. If something
-    seems important to you and it's not available here then please send
-    some email.</p>
-
-      <br>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminRoleRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <ul>
-       <li>Determine a good long-term structure for the aus.* hierarchy
-       <li>Accept newsgroup proposals which fit the policy and structure
-       <li>Final decision on newsgroup creation by voting
-       <li>Slowly purge obsolete groups
-       <li>Encourage news admins to respond to ausadmin newgroup and rmgroup messages
-      </ul>
-      <br>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminStructureRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <p>
-       In other words, what extensions to the structure does ausadmin consider
-       good or bad?
-      </p>
-
-      <p>Encouraged:</p>
-      <ul>
-       <li>aus.books extended by genre, or purpose (e.g. reviews)
-       <li>aus.cars extended by manufacturer (not model)
-       <li>aus.net.access extended by access technology name (e.g. adsl)
-       <li>aus.politics extended by major philosophy (e.g. socialism -- but no crossposting)
-       <li>aus.sport extensions include competitive activities
-       <li>aus.rec extended by recreational non-competitive activies
-       <li>aus.tv extended by genre or technology
-      </ul>
-
-      <p>Discouraged:</p>
-      <ul>
-       <li>Forsale groups outside the aus.ads hierarchy
-       <li>All binary groups are discouraged (post a URL instead)
-       <li>Regional subgroups, e.g. aus.motorcycles.wa (see wa.* and other regional hierarchies)
-       <li>aus.org subgroups
-       <li>aus.tv subgroups for particular shows
-      </ul>
-
-      <p>In general, be aware if the topic you are proposing can be discussed
-      from another perspective within an existing aus.* group, for example
-      aus.books.sf may be discussed in aus.sf already.
-      </p>
-      <br>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminPolicyRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <ul>
-       <li>Newsgroup creation only (no newsgroup deletions yet)
-       <li>Unmoderated newsgroup creation only
-       <li>Proposals considered by ausadmin to be frivolous or intended to "work the system" will be rejected
-       <li>No change of newsgroups from unmoderated to moderated (it caused a terrible storm last time)
-       <li>2-level newsgroups (aus.X) accepted only for major categories (e.g. science, arts, sport, humanities -- most of which exist already)
-       <li>3-level newsgroups (aus.X.Y) accepted only when parent exists (aus.X) or a sibling group already exists (aus.X.Z)
-       <li>The effect of the above two rules is that if you want a new group e.g. <i>aus.language.strine</i> with no existing parent and no siblings, then you need to first work on getting <i>aus.language</i> created, and that group will have to make do for strine discussions as well as other language discussions. You would then wait 1-2 years before proposing <i>aus.language.strine</i>.
-       <li>No change of charter yet, but if you wish to propose a charter for an existing group which does not already have a charter, email it in and ausadmin will consider accepting it directly as the new charter.
-      </ul>
-      <br>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminLinksRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <a href="ftp://rtfm.mit.edu/pub/usenet-by-hierarchy/aus/">aus.* FAQs on rtfm.mit.edu</a><br>
-      <a href="http://mirror.aarnet.edu.au/pub/rtfm/usenet-by-hierarchy/aus/">aus.* FAQs on mirror.aarnet.edu.au (local mirror of rtfm)</a><br>
-      <a href="http://www.newsreaders.com/">www.newsreaders.com</a> (very comprehensive USENET resource)<br>
-      <br>
-     </td>
-    </tr>
-EOF
-	return \@contents;
-}
-
-sub ausadminCreationProposalsRow {
-	my @contents;
-	push(@contents, <<EOF);
-    <tr>
-     <td>
-      <p>
-       Six Simple Steps to creation of a new newsgroup:
-       <br>
-       <ol>
-        <li>Make sure you really need a new newsgroup, and not a mailing list or a website or an existing newsgroup</li>
-	<li>Define the Topic and choose a group name which meets the ausadmin policy (above)</li>
-	<li>Discuss your idea on aus.net.news</li>
-	<li>Submit a formal proposal to ausadmin</li>
-	<li>Participate in the Request For Discussion (RFD) process</li>
-	<li>Ask ausadmin to put your proposal to the vote.</li>
-       </ol>
-     </td>
-    </tr>
-
-    <tr>
-     <td>
-      <p>
-       Further detail on the process is in the
-       <a href="/Faq/aus_faq">Group&nbsp;creation&nbsp;FAQ</a>.
-      </p>
-      <p>
-       You will also need this template:
-       <br>
-       <a href="Faq/RFD-template.txt">Template for writing an RFD (Request For Discussion)</a></br>
-      <br>
      </td>
     </tr>
 EOF
