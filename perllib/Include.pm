9fa83cbcdc8a1acbb9386218614180a06a9cb79f
Nick Andrew <nick@nick-andrew.net>
2003-06-25 01:20:39 +0000

    Allow in-document inclusion of various types of data
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@387 8ab3fd6c-811f-0410-acb7-fe4950304a8b

---
+++ perllib/Include.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+#	@(#) $Header$
+#
+#	Include a file into our output
+
+package Include;
+
+use strict;
+
+use Carp qw(confess);
+use IO::File qw(O_RDONLY O_WRONLY O_APPEND O_CREAT O_EXCL);
+
+sub html {
+	my $file = shift;
+
+	my $path = "$ENV{AUSADMIN_HOME}/config/$file";
+
+	if (!-f $path || ! -r _) {
+		$path = "$ENV{AUSADMIN_WEB}/Html/$file";
+	}
+
+	if (!-f $path || ! -r _) {
+		return undef;
+	}
+
+	my $fh = new IO::File($path, O_RDONLY);
+	die("Unable to open $path : $!") if (!defined $fh);
+
+	my @lines = <$fh>;
+
+	$fh->close();
+
+	return \@lines;
+}
+
+1;
