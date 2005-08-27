cd774f3c896074777539a7ec32c9422ffa2249f2
Nick Andrew <nick@nick-andrew.net>
2005-08-27 10:40:50 +0000

    Add simple templating engine
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@514 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- perllib/Include.pm
+++ perllib/Include.pm
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 #	@(#) $Header$
+#	vim:sw=4:ts=4:
 #
 #	Include a file into our output
 
@@ -32,5 +33,74 @@ sub html {
 
 	return \@lines;
 }
+
+sub new {
+	my $class = shift;
+
+	my $self = { @_ };
+	bless $self, $class;
+	return $self;
+}
+
+# ---------------------------------------------------------------------------
+# Recursively resolve a template.
+# Return answer as a string (HTML string, I suppose)
+# ---------------------------------------------------------------------------
+
+sub resolveFile {
+	my ($self, $filename) = @_;
+
+	# Grab the contents
+	my $lines = html($filename);
+	if (! $lines) {
+		return undef;
+	}
+
+	my $string = join('', @$lines);
+
+	# Identify and resolve markers in those lines
+	# Each marker looks like {{COMMAND|data|data,...}}
+	# The returned string must not contain any markers.
+	$string =~ s/{{(\w+)([^}]+)}}/resolveMarker($self,$1,$2)/eg;
+
+	return $string;
+}
+
+# ---------------------------------------------------------------------------
+# Recursively resolve the content of a marker
+# The returned string must not contain any markers.
+# ---------------------------------------------------------------------------
+
+sub resolveMarker {
+	my ($self, $command, $data) = @_;
+
+	my @args = split(/\|/, $data);
+	shift @args; # First one will be empty
+
+	if ($command eq 'uc') {
+		my $result = join(' ', @args);
+		return uc($result);
+	}
+	elsif ($command eq 'lc') {
+		my $result = join(' ', @args);
+		return lc($result);
+	}
+	elsif ($command eq 'FILE') {
+		my $filename = $args[0];
+		my $result = resolveFile($self, $filename);
+		if (! $result) {
+			return "<b>Nothing back from $filename</b>";
+		}
+		return $result;
+	}
+	elsif ($command eq 'A') {
+		# A|filename|description
+		return qq{<a href="c.cgi/$args[0]">$args[1]</a>};
+	}
+	else {
+		return "<b>Unresolved marker: $command</b>";
+	}
+}
+
 
 1;
