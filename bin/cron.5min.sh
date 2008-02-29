79d63f408550727851d556e2f164a6622a1374d6
Nick Andrew <ausadmin@aus.news-admin.org>
2008-02-29 23:26:45 +0000

    Tune up, comment, change filename of fail count file
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@559 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/cron.5min.sh
+++ bin/cron.5min.sh
@@ -6,26 +6,37 @@ HIER=${1:-aus}
 DATADIR=~/data/$HIER.data
 
 ts=`date '+%Y%m%d%H%M%S'`
-mrtg-newsgroups $DATADIR/checkgroups $DATADIR/mrtg-newsgroups-config.xml $DATADIR/mrtg-newsgroups-arrival-$HIER.log > $DATADIR/news-$ts.mrtg 2> $DATADIR/missing-groups
+TEMPFILE="~/tmp/news-$ts.mrtg"
+FAILCOUNT_FILE="$DATADIR/server-fail-count"
+
+# Create a file in my 5-line mrtg format containing the highwater article
+# numbers for each newsgroup in this hierarchy.
+
+mrtg-newsgroups $DATADIR/checkgroups $DATADIR/mrtg-newsgroups-config.xml $DATADIR/mrtg-newsgroups-arrival-$HIER.log > $TEMPFILE 2> $DATADIR/missing-groups
 s=$?
 
 if [ $s -ne 0 ] ; then
-	NG_FAIL=$(cat $DATADIR/ng-fail-count)
+	# Count how many successive failures talking to the server
+	# Alert every 20 failures
+	NG_FAIL=$(cat $FAILCOUNT_FILE)
 	NG_FAIL=$(expr $NG_FAIL + 1)
 	if [ $NG_FAIL -gt 20 ] ; then
 		echo Unable to connect to newsserver - $NG_FAIL times | mail -s error nick@tull.net
 		NG_FAIL=0
 	fi
-	echo $NG_FAIL > $DATADIR/ng-fail-count
-	rm -f ~/tmp/news-$ts.mrtg
+	echo $NG_FAIL > $FAILCOUNT_FILE
+	rm -f $TEMPFILE
 	exit 8
 fi
 
 # Success code path
 
-echo 0 > $DATADIR/ng-fail-count
-mv $DATADIR/news-$ts.mrtg ~/Mrtg/news-latest-$HIER.mrtg
+echo 0 > $FAILCOUNT_FILE
+mv $TEMPFILE ~/Mrtg/news-latest-$HIER.mrtg
 #mrtg ~/Mrtg/newsgroups.cfg
+
+# Create another file in my 5-line format containing the
+# arrival rate for new articles (avg per 2 hours and per 24)
 
 ~/bin/mrtg-newsgroups-arrival.pl -f $DATADIR/mrtg-newsgroups-arrival-$HIER.log > ~/Mrtg/arrival/news-latest-$HIER.mrtg
 
