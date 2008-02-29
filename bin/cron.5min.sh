5148f873300684b3a045738ad9f6b984d38c6f8c
Nick Andrew <ausadmin@aus.news-admin.org>
2008-02-29 23:19:24 +0000

    Clarify the execution paths for failure and success
    git-svn-id: http://svn.local.tull.net/svn/ausadmin-base/trunk@558 8ab3fd6c-811f-0410-acb7-fe4950304a8b

--- bin/cron.5min.sh
+++ bin/cron.5min.sh
@@ -9,11 +9,7 @@ ts=`date '+%Y%m%d%H%M%S'`
 mrtg-newsgroups $DATADIR/checkgroups $DATADIR/mrtg-newsgroups-config.xml $DATADIR/mrtg-newsgroups-arrival-$HIER.log > $DATADIR/news-$ts.mrtg 2> $DATADIR/missing-groups
 s=$?
 
-if [ $s -eq 0 ] ; then
-	echo 0 > $DATADIR/ng-fail-count
-	mv $DATADIR/news-$ts.mrtg ~/Mrtg/news-latest-$HIER.mrtg
-	#mrtg ~/Mrtg/newsgroups.cfg
-else
+if [ $s -ne 0 ] ; then
 	NG_FAIL=$(cat $DATADIR/ng-fail-count)
 	NG_FAIL=$(expr $NG_FAIL + 1)
 	if [ $NG_FAIL -gt 20 ] ; then
@@ -22,7 +18,14 @@ else
 	fi
 	echo $NG_FAIL > $DATADIR/ng-fail-count
 	rm -f ~/tmp/news-$ts.mrtg
+	exit 8
 fi
+
+# Success code path
+
+echo 0 > $DATADIR/ng-fail-count
+mv $DATADIR/news-$ts.mrtg ~/Mrtg/news-latest-$HIER.mrtg
+#mrtg ~/Mrtg/newsgroups.cfg
 
 ~/bin/mrtg-newsgroups-arrival.pl -f $DATADIR/mrtg-newsgroups-arrival-$HIER.log > ~/Mrtg/arrival/news-latest-$HIER.mrtg
 
